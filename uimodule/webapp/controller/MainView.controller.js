155859e1cfbc623470ba1c001aefe0b5d6eafe9b
Areliamavi <aresusy@hotmail.com>
2021-06-28 12:57:15 -0500

    Paises

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/bmore/Paises/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.bmore.Paises.controller.MainView", {});
+});
